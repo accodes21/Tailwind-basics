8bd63a2a31dfb3e43a4ffa123d668dd398e188f3
Aarya Chopkar <aaryachopkar@gmail.com>
2022-09-04 13:00:39 +0530

    Hover Text Changed

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,20 +6,20 @@ const SideBar = () => {
         <>
         <div className="fixed top-0 left-0 h-screen w-16
         flex flex-col bg-primary text-white shadow-lg">
-            <SideBarIcon icon={<FaFire size="28" />} />
+            <SideBarIcon icon={<FaFire size="28" />} text={"Fireship🔥"} />
             <Divider/>
-            <SideBarIcon icon={<BsPlus size="32" />} />
-            <SideBarIcon icon={<FaPython size="28" />} />
-            <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+            <SideBarIcon icon={<BsPlus size="32" />} text={"Add New Channel➕"} />
+            <SideBarIcon icon={<FaPython size="28" />} text={"Python🐍"}/>
+            <SideBarIcon icon={<BsFillLightningFill size="20" />} text={"Thunder⚡"} />
             <Divider/>
-            <SideBarIcon icon={<BsGearFill size="22" />} />
+            <SideBarIcon icon={<BsGearFill size="22" />} text={"Settings⚙️"} />
 
         </div>
         </>
     )
 };
 
-const SideBarIcon = ({icon, text = 'tooltip💡'}) => (
+const SideBarIcon = ({icon, text}) => (
     <div className="sidebar-icon group">
         {icon}
         <span className='sidebar-tooltip group-hover:scale-100'>
